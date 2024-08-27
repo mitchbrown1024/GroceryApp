ed679462d8e78c97d3a0243ad50de34681acfdae
Mitch Brown <mitchbrown1024@gmail.com>
2024-08-27 07:50:51 -0700

    Initial Commit

---
+++ Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by Mitch Brown on 2024-08-27.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroceryTests/GroceryTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryTests.swift
+//  GroceryTests
+//
+//  Created by Mitch Brown on 2024-08-27.
+//
+
+import Testing
+@testable import Grocery
+
+struct GroceryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
